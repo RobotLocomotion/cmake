9f9f53877a77b1d1dc9f7ac592ca6ddeb18f7f28
Russ Tedrake <russt@mit.edu>
2015-08-15 08:10:13 -0400

    support optional random unit tests

--- add_matlab_unit_tests.pl
+++ add_matlab_unit_tests.pl
@@ -1,4 +1,5 @@
 #!/usr/bin/perl -w
+# Note: this was intended to be a tool that works for general pods, but it looks like a few drake-specific commands have crept in...
 
 # Note: this must be run from the root directory of the pod, or the root must be passed in as the first argument
 if ($#ARGV>-1) {
@@ -9,6 +10,7 @@ if ($#ARGV>-1) {
 chomp($CMAKE_INSTALL_PREFIX = `cmake pod-build -N -L | grep CMAKE_INSTALL_PREFIX | cut -d "=" -f2`);
 chomp($CMAKE_SOURCE_DIR = `pwd`);
 chomp($POD_NAME = `cmake pod-build -N -L | grep POD_NAME | cut -d "=" -f2`);
+chomp($RANDOMIZE_UNIT_TESTS = `cmake pod-build -N -L | grep RANDOMIZE_UNIT_TESTS | cut -d "=" -f2`);
 
 if ($^O eq 'cygwin') {
   chomp($CMAKE_INSTALL_PREFIX = `cygpath -m $CMAKE_INSTALL_PREFIX`);
@@ -40,7 +42,11 @@ if ($? == 0) {
   #  $failcondition = "1";   # missing dependency => failure
     $failcondition = "~strncmp(ex.identifier,'Drake:MissingDependency',23)";  # missing dependency => pass
 
-    print $ctestfile "ADD_TEST($testname $perl_cmd \"$matlab_clean_cmd\" \"-r\" \"rng('shuffle'); rng_state=rng; disp(sprintf('To reproduce this test use rng(%d,''%s'')',rng_state.Seed,rng_state.Type)); disp(' '); addpath('$CMAKE_INSTALL_PREFIX/matlab'); addpath_$POD_NAME; global g_disable_visualizers; g_disable_visualizers=true; try, fevalPackageSafe('$test'); catch ex, disp(getReport(ex,'extended')); disp(' '); fprintf('<test_name>%s</test_name> <error_id>%s</error_id> <error_message>%s</error_message>','$testname',ex.identifier,ex.message); disp(' '); force_close_system; exit($failcondition); end; force_close_system; exit(0)\")\n";
+    print $ctestfile "ADD_TEST($testname $perl_cmd \"$matlab_clean_cmd\" \"-r\" \"";
+    if ($RANDOMIZE_UNIT_TESTS eq "ON") {
+      print $ctestfile "rng('shuffle'); rng_state=rng; disp(sprintf('To reproduce this test use rng(%d,''%s'')',rng_state.Seed,rng_state.Type)); disp(' '); "
+    }
+    print $ctestfile "addpath('$CMAKE_INSTALL_PREFIX/matlab'); addpath_$POD_NAME; global g_disable_visualizers; g_disable_visualizers=true; try, fevalPackageSafe('$test'); catch ex, disp(getReport(ex,'extended')); disp(' '); fprintf('<test_name>%s</test_name> <error_id>%s</error_id> <error_message>%s</error_message>','$testname',ex.identifier,ex.message); disp(' '); force_close_system; exit($failcondition); end; force_close_system; exit(0)\")\n";
 
     $props = "WORKING_DIRECTORY \"$testdir\" $props";
     print $ctestfile "SET_TESTS_PROPERTIES($testname PROPERTIES " . $props .")\n";
