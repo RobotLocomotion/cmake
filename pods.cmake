3ca531594097919f0c9784585ecb32fe0df15404
Russ Tedrake <russt@mit.edu>
2015-09-20 14:30:50 -0400

    fix for windows

--- pods.cmake
+++ pods.cmake
@@ -732,7 +732,7 @@ project(${POD_NAME})
 set(POD_NAME "${POD_NAME}" CACHE STRING "${POD_NAME}" )
 
 # PODs out-of-source build logic
-if (CMAKE_INSTALL_PREFIX STREQUAL "/usr/local" OR CMAKE_INSTALL_PREFIX STREQUAL "C:/Program Files") 
+if (CMAKE_INSTALL_PREFIX STREQUAL "/usr/local" OR CMAKE_INSTALL_PREFIX STREQUAL "C:/Program Files/Project") 
   find_file(_build_dir build PATHS ${PROJECT_SOURCE_DIR} ${PROJECT_SOURCE_DIR}/.. ${PROJECT_SOURCE_DIR}/../.. ${PROJECT_SOURCE_DIR}/../../.. ${PROJECT_SOURCE_DIR}/../../../..)
   if (_build_dir)
     set(CMAKE_INSTALL_PREFIX "${_build_dir}")
@@ -742,10 +742,6 @@ if (CMAKE_INSTALL_PREFIX STREQUAL "/usr/local" OR CMAKE_INSTALL_PREFIX STREQUAL 
   endif() 
 endif()
 message(STATUS CMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX})
-
-if (NOT CMAKE_BUILD_TYPE)
-  set(CMAKE_BUILD_TYPE "Release")
-endif()
 
 if ( WIN32 ) # convert to windows paths
    find_program(cygpath cygpath)
