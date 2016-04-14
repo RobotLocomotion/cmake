d207916250adc765355bba2d7bf379305facccbd
Bill Hoffman <bill.hoffman@kitware.com>
2016-04-14 11:55:21 -0400

    Extend matlab_add_unit_test to run arbitrary code for the test.
    This commit allows a custom matlab set of commands to be run
    as the test instead of runtests('matlab_file_name'). It also
    escapes strings for matlab commands to allow for complicated
    matlab commands to be passed into the tests.

--- FindMatlab.cmake
+++ FindMatlab.cmake
@@ -694,7 +694,6 @@ function(matlab_get_version_from_matlab_run matlab_binary_program matlab_list_ve
 
 endfunction()
 
-
 #.rst:
 # .. command:: matlab_add_unit_test
 #
@@ -716,6 +715,7 @@ endfunction()
 #     matlab_add_unit_test(
 #         NAME <name>
 #         UNITTEST_FILE matlab_file_containing_unittest.m
+#         [CUSTOM_MATLAB_COMMAND matlab_command_to_run_as_test]
 #         [UNITTEST_PRECOMMAND matlab_command_to_run]
 #         [TIMEOUT timeout]
 #         [ADDITIONAL_PATH path1 [path2 ...]]
@@ -731,6 +731,11 @@ endfunction()
 #   ``UNITTEST_FILE``
 #     the matlab unittest file. Its path will be automatically
 #     added to the Matlab path.
+#   ``CUSTOM_MATLAB_COMMAND``
+#     Matlab script command to run as the test.
+#     IIf this is not set, then the following is run:
+#     "runtests('matlab_file_name'), exit(max([ans(1,:).Failed]))
+#     matlab_file_name comes from UNITTEST_FILE without the .m.
 #   ``UNITTEST_PRECOMMAND``
 #     Matlab script command to be ran before the file
 #     containing the test (eg. GPU device initialisation based on CMake
@@ -744,6 +749,7 @@ endfunction()
 #   ``MATLAB_ADDITIONAL_STARTUP_OPTIONS``
 #     a list of additional option in order
 #     to run Matlab from the command line.
+#     -nosplash -nodesktop -nodisplay are always added.
 #   ``TEST_ARGS``
 #     Additional options provided to the add_test command. These
 #     options are added to the default options (eg. "CONFIGURATIONS Release")
@@ -758,8 +764,9 @@ function(matlab_add_unit_test)
   endif()
 
   set(options NO_UNITTEST_FRAMEWORK)
-  set(oneValueArgs NAME UNITTEST_PRECOMMAND UNITTEST_FILE TIMEOUT)
-  set(multiValueArgs ADDITIONAL_PATH MATLAB_ADDITIONAL_STARTUP_OPTIONS TEST_ARGS)
+  set(oneValueArgs NAME UNITTEST_FILE TIMEOUT WORKING_DIRECTORY)
+  set(multiValueArgs UNITTEST_PRECOMMAND ADDITIONAL_PATH
+    MATLAB_ADDITIONAL_STARTUP_OPTIONS TEST_ARGS CUSTOM_TEST_COMMAND)
 
   set(prefix _matlab_unittest_prefix)
   cmake_parse_arguments(${prefix} "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN} )
@@ -768,17 +775,29 @@ function(matlab_add_unit_test)
     message(FATAL_ERROR "[MATLAB] The Matlab test name cannot be empty")
   endif()
 
+  # escape matlab commands so they can be passed to add_test
+  # and to cmake -P script.
+
+  string(REPLACE "\"" "" custom_test_cmd_clean "${${prefix}_CUSTOM_TEST_COMMAND}")
+  string(REPLACE ";" "\\;" custom_test_cmd_clean "${custom_test_cmd_clean}")
+  string(REPLACE "\"" "" unittest_precmd_clean "${${prefix}_UNITTEST_PRECOMMAND}")
+  string(REPLACE ";" "\\;" unittest_precmd_clean "${unittest_precmd_clean}")
+  string(REPLACE " " "\ " unittest_precmd_clean "${unittest_precmd_clean}")
+  string(REPLACE ";" "\\;" additional_path_clean "${${prefix}_ADDITIONAL_PATH}")
+
   add_test(NAME ${${prefix}_NAME}
            COMMAND ${CMAKE_COMMAND}
             -Dtest_name=${${prefix}_NAME}
-            -Dadditional_paths=${${prefix}_ADDITIONAL_PATH}
+            -Dadditional_paths=${additional_path_clean}
             -Dtest_timeout=${${prefix}_TIMEOUT}
             -Doutput_directory=${_matlab_temporary_folder}
+	    -Dworking_directory=${${prefix}_WORKING_DIRECTORY}
             -DMatlab_PROGRAM=${Matlab_MAIN_PROGRAM}
             -Dno_unittest_framework=${${prefix}_NO_UNITTEST_FRAMEWORK}
             -DMatlab_ADDITIONNAL_STARTUP_OPTIONS=${${prefix}_MATLAB_ADDITIONAL_STARTUP_OPTIONS}
             -Dunittest_file_to_run=${${prefix}_UNITTEST_FILE}
-            -Dcmd_to_run_before_test=${${prefix}_UNITTEST_PRECOMMAND}
+	    -Dcustom_Matlab_test_command=${custom_test_cmd_clean}
+            -Dcmd_to_run_before_test=${unittest_precmd_clean}
             -P ${_FindMatlab_SELF_DIR}/MatlabTestsRedirect.cmake
            ${${prefix}_TEST_ARGS}
            ${${prefix}_UNPARSED_ARGUMENTS}
